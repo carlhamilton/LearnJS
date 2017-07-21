fa1a04b76ad271121fa1a3054b859556c1e7bcfe
Carl Matthew Hamilton <info@chtsi.uk>
2017-07-21 23:36:13 +0100

    update to add zombie biting

--- NewJs/scripts/scenario.js
+++ NewJs/scripts/scenario.js
@@ -9,8 +9,19 @@ function randomNumber(range){
 
 alert(beginningScenarios[randomNumber(beginningScenarios.length -1 ) ] );
 
-var weaponlist = ["shovel","crossbow","baseball bat","rubber chicken"];
+var weaponlist = ["shovel ","crossbow ","baseball bat ","rubber chicken "];
 
 var weapon = weaponlist[ randomNumber(weaponlist.length -1 ) ];
 
 alert("As it is a zombie apocoloypse, you decide to search for a weapon first. After surveying your surroundings you notice and grab a " + weapon + ".");
+
+alert("Suddenly a zombie bursts through the door! You ready your " + weapon + "and advance towards the zombie.");
+
+var survival = randomNumber(2);
+
+if(survival === 0){
+    alert("The zombie has bitten you which means you will die and turn into a zombie and want brains, yum yum!!!");
+}
+else if(survival > 0){
+    alert("You kill the zombie with your " + weapon + "well done, you win.");
+}
