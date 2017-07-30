05121ee197bf3fb3b6a7606cf7a7ab5e93f59464
Carl Matthew Hamilton <info@chtsi.uk>
2017-07-30 23:23:41 +0100

    k
    k

--- Newtry/scripts/script.js
+++ Newtry/scripts/script.js
@@ -1,36 +1,21 @@
 'use strict';
-/*global foo */
-var cat = {
-    name: { first: 'Fluffy', last: 'LaBeouf' },
-    color: 'White'
-};
 
-Object.defineProperty(cat, 'fullName', {
-    get: function() {
-        return this.name.first + ' ' + this.name.last;
-    },
-    set: function(value) {
-        var nameParts = value.split(' ');
-        this.name.first = nameParts[0];
-        this.name.last = nameParts[1];
+class Animal {
+    constructor(voice) {
+        this.voice = voice || 'grunt';
     }
-
-
-});
-cat.fullName = 'Muffin Top';
-display(cat.fullName);
-display(cat.name.first);
-display(cat.name.last);
-
-var arr = ['red', 'blue', 'green'];
-Object.defineProperty(Array.prototype, 'last', {
-    get: function() {
-        return this[this.length - 1];
+    speak() {
+        display(this.voice);
     }
-});
+}
 
-var last = arr.last;
-var arr2 = ['one', 'two', 'three'];
-display(last);
-display(arr.last);
-display(arr2.last);
+class Cat extends Animal {
+    constructor(name, color) {
+        super('Meow');
+        this.name = name;
+        this.color = color;
+    }
+}
+
+var fluffy = new Cat('Fluffy', 'White');
+fluffy.speak();
